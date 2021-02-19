0fd4c1b72cc2fc5aabae8f60de33580b1b9cfefb
Arpita <40335166+Arpita-1998@users.noreply.github.com>
2021-02-19 20:35:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PagesContainer from './components/PagesContainer';
+import 'antd/dist/antd.css';
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className={"app-container"}>
+      <PagesContainer/>
+    </div>
+  );
+}
+
+export default App;
